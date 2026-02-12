2c1adddcac161808e78bb9b91b7968aaea741024
Danilomello41 <Danilomello.mello@hotmail.com>
2026-02-12 01:35:16 +0000

    Atualização pib/densidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
+    int main() {
 
-int main() {
-  char e1 [50];
+    char e1 [50];
     char e2 [50];
     char c1 [50];
     char c2 [50];
@@ -16,6 +16,11 @@ int main() {
     float PIB2;
     int pt1;
     int pt2;
+    float ppc1;
+    float ppc2; 
+    float dp1;
+    float dp2;
+    
         
         printf("Codigo da Carta: %s \n", cod1);
 
@@ -63,24 +68,32 @@ int main() {
 
         printf(" -----------------------------------\n\n");
 
+        ppc1 = (float)PIB1 / p1;
+        ppc2 = (float)PIB2 / p2;
+        dp1 = (float)p1 / a1;
+        dp2 = (float)p2 / a2;
+        
+
         printf("Codigo: %s \n", cod1);
         printf("Estado: %s \n", e1);
         printf("Cidade: %s \n", c1);
         printf("População: %d \n", p1);
-        printf("Area: %.2f km² \n", a1);
-        printf("PIB: %.2f  \n", PIB1);
-        printf("Pontos Turisticos: %d \n\n", pt1);
+        printf("Area: %.3f km² \n", a1);
+        printf("PIB: R$%.2f  \n", PIB1);
+        printf("Pontos Turisticos: %d \n", pt1);
+        printf("Densidade Populacional: %.2f \n",dp1);
+        printf("PIB per capita: R$%.2f \n",ppc1);
         printf("------------------------------------\n\n");
         printf("Codigo: %s \n", cod2);
         printf("Estado: %s \n", e2);
         printf("Cidade: %s \n", c2);
         printf("População: %d \n", p2);
-        printf("Area: %.2f km² \n", a2);
-        printf("PIB: %.2f  \n", PIB2);
-        printf("Pontos Turisticos: %d \n\n", pt2);
+        printf("Area: %.3f km² \n", a2);
+        printf("PIB: R$ %.2f  \n", PIB2);
+        printf("Pontos Turisticos: %d \n", pt2);
+        printf("Densidade Populacional: R$ %.2f  \n",dp2);
+        printf("PIB per capita: %.2f \n",ppc2);
 
     }
 
-
-
-
+    
